ac32447135b2eeb1fa75ca975911ad43f2a4d0bf
Ankita7033 <137888030+Ankita7033@users.noreply.github.com>
2024-01-16 16:47:53 +0530

    Add files via upload

---
+++ pattern_1.py
@@ -0,0 +1,109 @@
+a=int(input("Enter the number: "))
+if a==0:
+   for x in range(0,3):
+        print("*",end=" ")
+   print(" ")
+   for x in range(0,2):
+        print("*   *")
+   for x in range(0,3):
+        print("*",end=" ")
+if a==1:
+    for x in range(0,5): 
+        print("*")
+elif a==2:
+    for x in range(0,3):
+        print("*",end="")
+    print(" ")
+    for x in range(0,1):
+        print("  *")
+    
+    for x in range(0,3): 
+         print("*",end="")
+    print(" ")
+    for x in range(0,1):
+        print("*")
+         
+    for x in range(0,3): 
+        print("*",end="")
+elif a==3:
+    for x in range(0,3):
+        print("*",end="")
+    print(" ")
+    for x in range(0,1):
+        print("  *")
+    for x in range(0,3):
+        print("*",end="")
+    print(" ")
+    for x in range(0,1):
+        print("  *")
+    for x in range(0,3):
+        print("*",end="")
+elif a==4:
+    for x in range(0,2): 
+        print("*   *")
+    for x in range(0,3):
+        print("*",end=" ")
+    print("")
+    for x in range(0,2): 
+        print("    *")
+elif a==5:
+    for x in range(0,3):
+        print("*",end=" ")
+    print(" ")
+    for x in range(0,2):
+        print("*")
+    for x in range(0,3):
+        print("*",end=" ")
+    print(" ")
+    for x in range(0,2):
+        print("    *")
+    for x in range(0,3):
+        print("*",end=" ")
+
+elif a==6:
+    for x in range(0,3):
+        print("*",end=" ")
+    print(" ")
+    for x in range(0,1):
+        print("*")
+    for x in range(0,3):
+        print("*",end=" ")
+    print(" ")    
+    for x in range(0,1):
+        print("*   *")
+    for x in range(0,3):
+        print("*",end=" ")
+elif a==7:
+    for x in range(0,3):
+        print("*",end=" ")
+    print(" ")
+    for x in range(0,3):
+        print("    *")
+elif a==8:
+    for x in range(0,3):
+        print("*",end=" ")
+    print(" ")
+    for x in range(0,1):
+        print("*   *")
+    for x in range(0,3):
+        print("*",end=" ")
+    print(" ")
+    for x in range(0,1):
+        print("*   *")
+    for x in range(0,3):
+        print("*",end=" ")
+elif a==9:
+    for x in range(0,3):
+        print("*",end=" ")
+    print(" ")
+    for x in range(0,1):
+        print("*   *")
+    for x in range(0,3):
+        print("*",end=" ")
+    print(" ")
+    for x in range(0,1):
+        print("    *")
+    for x in range(0,3):
+        print("*",end=" ")
+else:
+    print("Enter Single Digit Whole Number")
